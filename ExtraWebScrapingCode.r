b1be8d016d64709bb653ed19ac2eb9a63d3812f8
Candy Rae Boatwright <cboatw2@clemson.edu>
2025-03-21 05:26:44 -0400

    Successfully used example URL to get first record of results. Changed example to my query. No results retuned for SC newspapers from 1796 to 1920 with the search term nullificaiton.

--- ExtraWebScrapingCode.r
+++ ExtraWebScrapingCode.r
@@ -11,3 +11,98 @@ if(status_code(request) == 200) {
 }  else {
     print ("Request failed. Try again!")
 }
+
+
+#Pulled from WebScraping.r: when copying corrected URL from copilot
+#Load Libraries:
+#library(jsonlite)
+#library(httr)
+#library(tidyverse)
+
+#res <- 
+#Run GET()
+#Status: 200 #successful request (400 is failed)
+
+#content(data, as = “test”)
+#fromJSON(content)
+
+#ocr<- json_data$items
+#print (ocr$ocr_eng[15])
+
+
+#install.packages('internetarchive', repos = c('https://ropensci.r-universe.dev', 'https://cloud.r-project.org'))
+#library(internetarchive)
+
+#install.packages("tidyverse")
+
+#install.packages("conflicted")
+
+#Resolve conflicts created by using tidyverse with other packages
+conflicted::conflict_prefer("filter", "dplyr")
+conflicted::conflict_prefer("lag", "dplyr")
+conflicted::conflict_prefer("flatten", "purrr")
+conflicted::conflict_prefer("simplify", "purrr")
+
+#First attempt at URL:
+#request <- GET(" https://chroniclingamerica.loc.gov/search/titles/results/?terms=nullification")
+
+#Changed URL using results from Google search: how to search for a specific term in South Carolina newspapers using Chronicling America's API:
+
+#locgov_url_search <- ("http://www.loc.gov/collections/chronicling-america/?location_state=southcarolina&qs=Nullification&end_data=1945-12-31&fo=json")
+#locgov_url_search <- ("https://chroniclingamerica.loc.gov/search/pages/results/?state=southcarolina&dateFilterType=yearRange&date1=1796&date2=1912&sort=date&andtext=trade&format=json")
+
+# Using exact example from https://studentwork.prattsi.org/dh/2019/05/13/getting-data-for-digital-humanities-with-apis/:
+#locgov_url_search <- ("https://chroniclingamerica.loc.gov/search/pages/results/?state=Alaska&dateFilterType=yearRange&date1=1912&date2=1916&sort=date&andtext=suffrage&format=json")
+
+request <- GET("http://www.loc.gov/collections/chronicling-america/?location_state=southcarolina&qs=Nullification&end_data=1945-12-31&fo=json")
+
+#Checks to see if search was successful. Prints raw content of search in order to debug from lexical error I was receiving previously.
+#if (status_code(request) == 200) {
+  # Print raw content for debugging
+#  raw_content <- content(request, as = "text")
+#  print(raw_content)
+
+  # Parse the content as JSON
+#  json_data <- fromJSON(raw_content)
+  
+  # Print out the number of items per page
+#  print(json_data$itemsPerPage)
+  
+  # Extract specific data
+#  ocr <- json_data$items
+#  print(ocr[[15]]$ocr_eng) # Adjust the index and field as needed
+#} else {
+#  print("Request failed. Try again!")
+#}
+
+
+install.packages ("internetarchive", repos = c("https://ropensci.r-universe.dev", "https://cloud.r-project.org"))
+library(internetarchive)
+
+
+# Define your query URL
+#locgov_url_search <- ("http://www.loc.gov/collections/chronicling-america/?location_state=southcarolina&qs=Nullification&end_data=1945-12-31&fo=json")
+#locgov_url_search <- ("https://chroniclingamerica.loc.gov/search/pages/results/?state=southcarolina&dateFilterType=yearRange&date1=1796&date2=1912&sort=date&andtext=trade&format=json")
+
+#Using exact example from https://studentwork.prattsi.org/dh/2019/05/13/getting-data-for-digital-humanities-with-apis/:
+#locgov_url_search <- ("https://chroniclingamerica.loc.gov/search/pages/results/?state=Alaska&dateFilterType=yearRange&date1=1912&date2=1916&sort=date&andtext=suffrage&format=json")
+
+locgov_url_search <- paste0("https://chroniclingamerica.loc.gov/search/pages/results/?state=SouthCarolina&dateFilterType=yearRange&date1=1796&date2=1920&sort=date&andtext=nullification&format=json"&page=2)
+
+# Run the query using the API
+api_query <- GET(locgov_url_search)
+
+#Check if query was successful
+if (status_code(api_query) == 200) {
+
+# Tell R to read the results as JSON
+search_result <- fromJSON(content(api_query, as = "text", encoding = "UTF-8"))
+ print(search_result)
+} else {
+  print("Request failed. Try again!")
+}
+
+#Look at first record of results
+first_result <- search_result["results"][0]
+print(first_result)
+
