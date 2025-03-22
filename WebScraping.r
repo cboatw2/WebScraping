315085238242d68a6064ade3fbe68aa42b20c4a6
Candy Rae Boatwright <cboatw2@clemson.edu>
2025-03-22 05:40:14 -0400

    Added code to display total number of search results. Matches the number when I do a search on the site.

--- WebScraping.r
+++ WebScraping.r
@@ -13,7 +13,7 @@ library(internetarchive)
 
 # Define your query URL: search from the first year SC newspapers are available to the last year before the Civil War for the term "nullification"
 
-locgov_url_search <- ("https://chroniclingamerica.loc.gov/search/pages/results/?state=South+Carolina&dateFilterType=yearRange&date1=1836&date2=1860&sort=date&andtext=nullification&format=json")
+locgov_url_search <- ("https://chroniclingamerica.loc.gov/search/pages/results/?state=South+Carolina&dateFilterType=yearRange&date1=1836&date2=1860&sort=date&andtext=commerce&format=json")
 
 # Run the query using the API
 api_query <- GET(locgov_url_search)
@@ -24,10 +24,15 @@ if (status_code(api_query) == 200) {
   # Tell R to read the results as JSON
   search_result <- fromJSON(content(api_query, as = "text", encoding = "UTF-8"))
   print(search_result)
+
+  #Print the total number of search results
+  total_results <- search_result$totalItems
+  print(paste("Total number of results:", total_results))
+
 } else {
   print("Request failed. Try again!")
 }
 
 # Look at first record of results
-first_result <- search_result$results[[1]]
-print(first_result)
+#first_result <- search_result$results[[1]]
+#print(first_result)
